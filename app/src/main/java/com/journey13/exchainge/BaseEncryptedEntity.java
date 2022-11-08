dff889720b1df14d480615a41b9e82d2909e2399
shoto13 <wilsonrc@pm.me>
2022-11-08 19:03:10 +0000

    Creation of both base encrypted entity and local encrypted user

---
+++ app/src/main/java/com/journey13/exchainge/BaseEncryptedEntity.java
@@ -0,0 +1,21 @@
+package com.journey13.exchainge;
+
+import org.whispersystems.libsignal.SignalProtocolAddress;
+
+public abstract class BaseEncryptedEntity {
+    protected final int registrationId;
+    protected final SignalProtocolAddress address;
+
+    protected BaseEncryptedEntity(int registrationId, SignalProtocolAddress address) {
+        this.registrationId = registrationId;
+        this.address = address;
+    }
+
+    public int getRegistrationId() {
+        return registrationId;
+    }
+
+    public SignalProtocolAddress getSignalProtocolAddress() {
+        return address;
+    }
+}
